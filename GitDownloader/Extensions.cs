94967bf682a8503bb57c7e6b50afdf0dcd028bf6
ecsousa <ecsousa@gmail.com>
2014-07-16 17:34:13 -0300

    GitDownloader project
    GitDownloader is a console project that will download a file from
    git repository by creating a local repository cache, and then
    will retrive the file from this repositry by its ID.
    
    It will be used on further indexer version so it can be used with
    any kind and any git provider service.

---
+++ GitDownloader/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Git.Credential.WinStore
+{
+    static class Extensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue def)
+        {
+            TValue ret;
+            if (!self.TryGetValue(key, out ret))
+            {
+                return def;
+            }
+            return ret;
+        }
+    }
+}
---
+++ GitDownloader/Program.cs
@@ -0,0 +1,122 @@
+﻿using Git.Credential.WinStore;
+using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GitDownloader
+{
+    static class Extensions
+    {
+        public static string SHA1(this string inputString)
+        {
+            using(var sha1 = new System.Security.Cryptography.SHA1Managed())
+            {
+                var buffer = System.Text.Encoding.UTF8.GetBytes(inputString);
+                var hash = sha1.ComputeHash(buffer);
+
+                return BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 3)
+            {
+                Console.Error.WriteLine("GitDownloader requires 3 parameters:");
+                Console.Error.WriteLine(" 1. Repository URI");
+                Console.Error.WriteLine(" 2. File ID");
+                Console.Error.WriteLine(" 3. Output path");
+            }
+
+            var repositoryUri = args[0];
+            var fileID = args[1];
+            var output = args[2];
+
+            var repositoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GitDownloader", repositoryUri.SHA1());
+
+
+            if(!Repository.IsValid(repositoryPath))
+            {
+                Console.Error.WriteLine("Could not file repository on '{0}'. Cloning it.", repositoryPath);
+                try
+                {
+                    try
+                    {
+                        Repository.Clone(repositoryUri, repositoryPath, new CloneOptions { Checkout = false });
+                    }
+                    catch(Exception ex)
+                    {
+                        if(ex.Message == "Request failed with status code: 401")
+                        {
+                            Repository.Clone(repositoryUri, repositoryPath, new CloneOptions { Checkout = false, Credentials = RetrieveCredentials(repositoryUri) });
+                        }
+                        else throw;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.Error.WriteLine("Error coling repository: {0}", ex.Message);
+                    Environment.ExitCode = 1;
+                    return;
+                }
+            }
+
+            using(var repository = new Repository(repositoryPath))
+            {
+                var blob = repository.Lookup<Blob>(fileID);
+
+                if(blob == null)
+                {
+                    Console.Error.WriteLine("Object {0} not found on local repository. Fetching origin remote for updates.", fileID);
+                    try
+                    {
+                        repository.Fetch("origin");
+                    }
+                    catch(Exception ex)
+                    {
+                        if(ex.Message == "Request failed with status code: 401")
+                        {
+                            repository.Fetch("origin", new FetchOptions { Credentials = RetrieveCredentials(repositoryUri) });
+                        }
+                        else throw;
+                    }
+                    blob = repository.Lookup<Blob>(fileID);
+                }
+
+                if(blob == null)
+                {
+                    Console.Error.WriteLine("Object {0} not found on local repository after fetch. Exiting.", fileID);
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                using(var file = File.Create(output))
+                using(var content = blob.GetContentStream())
+                    content.CopyTo(file);
+            }
+
+        }
+
+        private static Credentials RetrieveCredentials(string repositoryUri)
+        {
+            var completeUri = new Uri(repositoryUri);
+
+            var info = Store.GetCommand(new Uri(completeUri, "/")).ToDictionary(
+                t => t.Item1, t => t.Item2);
+
+            return new UsernamePasswordCredentials
+            {
+                Username = info["username"],
+                Password = info["password"]
+            };
+
+            Store.WriteGitParameters(info);
+        }
+    }
+}
